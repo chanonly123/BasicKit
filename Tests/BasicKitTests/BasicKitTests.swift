ce458ec4ab10dec04498bec94b86cca6c8da9840
Chandan Karmakar <chan.only.123@gmail.com>
2024-03-28 19:11:57 +0530

    updates

--- Tests/BasicKitTests/BasicKitTests.swift
+++ Tests/BasicKitTests/BasicKitTests.swift
@@ -1,12 +1,29 @@
 import XCTest
 @testable import BasicKit
 
-final class BasicKitTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+struct User: Codable, Equatable {
+    var _id: String
+    var userName: String
+}
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+extension SomeDefaultsKey {
+    static var user: SomeDefaultsKey<User> { SomeDefaultsKey<User>("user", defaultVal: nil) }
+    static var array: SomeDefaultsKey<[Int]> { SomeDefaultsKey<[Int]>("array", defaultVal: nil) }
+    static var value: SomeDefaultsKey<Int> { SomeDefaultsKey<Int>("value", defaultVal: nil) }
+}
+
+final class BasicKitTests: XCTestCase {
+    let defaults = SomeDefaults()
+    
+    func testSomeDefaults() throws {
+        let user = User(_id: "abc", userName: "xyz")
+        defaults[.user] = user
+        XCTAssert(defaults[.user] == user, "User shoud be same")
+        
+        defaults[.value] = 10
+        XCTAssert(defaults[.value] == 10, "value shoud be same")
+        
+        defaults[.array] = [1,2,3,4,5]
+        XCTAssert(defaults[.array] == [1,2,3,4,5], "array shoud be same")
     }
 }
